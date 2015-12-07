407eb324bf76943adde7383a6e0b118d545dd3d9
ajnouri <ajn.bin@gmail.com>
2015-12-07 02:51:53 +0100

    Add running container id's to the ip lister

--- iplist.sh
+++ iplist.sh
@@ -5,7 +5,7 @@ echo "Running container default IPs"
 for id in $(docker ps --format "{{.ID}}")
 do
     echo -n "$(docker inspect  -f '{{ .Name }}' $id) :"
-    echo "  (eth0) $(docker inspect -f '{{ .NetworkSettings.IPAddress }}' $id )"
+    echo "  (eth0) [$id] $(docker inspect -f '{{ .NetworkSettings.IPAddress }}' $id )"
 done
 echo ""
 
